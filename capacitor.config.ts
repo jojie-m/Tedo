9e2634758ca75319c72d43678934c351e9226f18
George Mburu (meetjojie) <mburugeorge692@gmail.com>
2023-04-20 14:28:10 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tedo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
