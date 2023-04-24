893b8a58643bbfbd26ecf0459d32e89b2f17124f
George Mburu (meetjojie) <70822553+jojie-m@users.noreply.github.com>
2023-04-24 22:04:57 +0300

    some minor changes

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormsModule } from '@angular/forms';
+//**//
+import { FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms'; //fixed that
+//**//
 import { IonicModule } from '@ionic/angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -10,9 +12,10 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './contact.page.html',
   styleUrls: ['./contact.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule ]//<==
 })
 export class ContactPage implements OnInit {
+  //**//
   contactPage = new FormGroup({});
   constructor(private formBuilder: FormBuilder) { }
   ngOnInit() {
